3745e70ddee1faa111daae58d3d8c10dd5f2b7e1
Jhonnatan Guerrero <Jhonnatanguerrerom@gmail.com>
2024-12-23 00:27:01 -0800

    feat(init): add tailwindcss

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -32,10 +32,10 @@ export default function Home() {
           priority
         />
         <ol>
-          <li>
+          <li className="text-green-800">
             Get started by editing <code>apps/web/app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
+          </li >
+          <li className="text-green-800">Save and see your changes instantly.</li>
         </ol>
 
         <div className={styles.ctas}>
